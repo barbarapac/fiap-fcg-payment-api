70c4892d5213db9e420afaf538279ce7c5522f9b
Winicius Torres <rm366458@fiap.com.br>
2026-01-06 22:27:41 -0300

    feat: inclusão do correlation-ID

--- src/Fiap.FCG.Payment.WebApi/_Shared/RequestLoggingScopeMiddleware.cs
+++ src/Fiap.FCG.Payment.WebApi/_Shared/RequestLoggingScopeMiddleware.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 
 namespace Fiap.FCG.Payment.WebApi._Shared;
 
@@ -22,13 +23,15 @@ public class RequestLoggingScopeMiddleware
 
     public async Task Invoke(HttpContext context)
     {
-        var activity = Activity.Current;
-
         var correlationId =
             context.Request.Headers.TryGetValue(CorrelationHeader, out var headerValue)
             && !string.IsNullOrWhiteSpace(headerValue)
                 ? headerValue.ToString()
-                : (activity?.TraceId.ToString() ?? context.TraceIdentifier);
+                : (Activity.Current?.TraceId.ToString() ?? context.TraceIdentifier);
+
+        Activity.Current?.SetTag("CorrelationId", correlationId);
+
+        var activity = Activity.Current;
 
         context.Response.OnStarting(() =>
         {
@@ -38,15 +41,15 @@ public class RequestLoggingScopeMiddleware
 
         using var scope = _logger.BeginScope(new[]
         {
-            new KeyValuePair<string, object?>("CorrelationId", correlationId),
-            new KeyValuePair<string, object?>("TraceId", activity?.TraceId.ToString()),
-            new KeyValuePair<string, object?>("SpanId", activity?.SpanId.ToString()),
-            new KeyValuePair<string, object?>("HttpMethod", context.Request.Method),
-            new KeyValuePair<string, object?>("HttpPath", context.Request.Path.ToString())
-        });
+        new KeyValuePair<string, object?>("CorrelationId", correlationId),
+        new KeyValuePair<string, object?>("TraceId", activity?.TraceId.ToString()),
+        new KeyValuePair<string, object?>("SpanId", activity?.SpanId.ToString()),
+        new KeyValuePair<string, object?>("HttpMethod", context.Request.Method),
+        new KeyValuePair<string, object?>("HttpPath", context.Request.Path.ToString())
+    });
 
         var sw = Stopwatch.StartNew();
-        _logger.LogInformation("Requisi��o HTTP iniciada");
+        _logger.LogInformation("Requisição HTTP iniciada");
 
         try
         {
@@ -54,16 +57,17 @@ public class RequestLoggingScopeMiddleware
             sw.Stop();
 
             _logger.LogInformation(
-                "Requisi��o HTTP finalizada. StatusCode: {StatusCode}. DuracaoMs: {DuracaoMs}",
+                "Requisição HTTP finalizada. StatusCode: {StatusCode}. DuracaoMs: {DuracaoMs}",
                 context.Response.StatusCode,
                 sw.ElapsedMilliseconds);
         }
-        catch
+        catch (Exception ex)
         {
             sw.Stop();
 
             _logger.LogError(
-                "Falha ao processar requisi��o HTTP. DuracaoMs: {DuracaoMs}",
+                ex,
+                "Falha ao processar requisição HTTP. DuracaoMs: {DuracaoMs}",
                 sw.ElapsedMilliseconds);
 
             throw;
